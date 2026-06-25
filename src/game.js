9cad72571eca7bc8c8ddc6e3cca07936f2fae24e
kangseok0427 <  >
2026-06-25 11:38:11 +0900

    gaon: 06/25 11:38

--- src/game.js
+++ src/game.js
@@ -14,6 +14,30 @@ window.addEventListener('keyup', (e) => {
     keys[e.code] = false;
 });
 
+class Bullet {
+    constructor(x, y, angle) {
+        this.x = x;
+        this.y = y;
+        this.velocity = {
+            x: Math.cos(angle) * 7,
+            y: Math.sin(angle) * 7
+        };
+        this.radius = 2;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = '#ffff00';
+        ctx.fill();
+    }
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
 class Ship {
     constructor() {
         this.x = canvas.width / 2;
@@ -23,7 +47,9 @@ class Ship {
         this.velocity = { x: 0, y: 0 };
         this.rotationSpeed = 0.05;
         this.acceleration = 0.15;
-        this.friction = 0.99; // Space friction (slight)
+        this.friction = 0.99;
+        this.lastShot = 0;
+        this.shootCooldown = 250;
     }
 
     draw() {
@@ -31,11 +57,11 @@ class Ship {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
         
-        // Draw ship body
         ctx.beginPath();
         ctx.moveTo(20, 0);
         ctx.lineTo(-15, -15);
         ctx.lineTo(-10, 0);
+        ctx.lineTo(-15, 1s);
         ctx.lineTo(-15, 15);
         ctx.closePath();
         
@@ -43,7 +69,6 @@ class Ship {
         ctx.lineWidth = 2;
         ctx.stroke();
         
-        // Engine glow if moving
         if (keys['KeyW']) {
             ctx.beginPath();
             ctx.moveTo(-12, -5);
@@ -57,7 +82,6 @@ class Ship {
     }
 
     update() {
-        // Rotation
         if (keys['KeyA']) {
             this.angle -= this.rotationSpeed;
         }
@@ -65,17 +89,19 @@ class Ship {
             this.angle += this.rotationSpeed;
         }
 
-        // Acceleration
         if (keys['KeyW']) {
             this.velocity.x += Math.cos(this.angle) * this.acceleration;
             this.velocity.y += Math.sin(this.angle) * this.acceleration;
         }
 
-        // Apply velocity
+        // Shooting logic
+        if (keys['Space'] && Date.now() - this.lastShot > this.shootCooldown) {
+            bullets.push(new Bullet(this.x, this.y, this.angle));
+            this.lastShot = Date.now();
+        }
+
         this.x += this.velocity.x;
         this.y += this.velocity.y;
-
-        // Apply friction
         this.velocity.x *= this.friction;
         this.velocity.y *= this.friction;
 
@@ -88,14 +114,32 @@ class Ship {
 }
 
 const player = new Ship();
+const bullets = [];
 
 function animate() {
     requestAnimationFrame(animate);
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)'; // Motion blur effect
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     player.update();
     player.draw();
+
+    // Update and draw bullets
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        const bullet = bullets[i];
+        bullet.update();
+        bullet.draw();
+
+        // Remove bullets off-screen
+        if (
+            bullet.x < 0 || 
+            bullet.x > canvas.width || 
+            bullet.y < 0 || 
+            bullet.y > canvas.height
+        ) {
+            bullets.splice(i, 1);
+        }
+    }
 }
 
 window.addEventListener('resize', () => {
