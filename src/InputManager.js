2d2b6a6bdb413f1cbe805c6f2aaf68bdccc4b200
kangseok0427 <  >
2026-06-25 19:00:15 +0900

    gaon: 06/25 19:00

--- src/InputManager.js
+++ src/InputManager.js
@@ -1,12 +1,17 @@
 class InputManager {
     constructor() {
         this.keys = {};
+        this.backObjs = [];
         window.addEventListener('keydown', (e) => {
             this.keys[e.code] = true;
         });
         window.addEventListener('keyup', (e) => {
             this.keys[e.code] = false;
         });
+    }
+
+    initObjs(stars) {
+        this.backObjs = [...stars];
     }
 
     isKeyPressed(code) {
