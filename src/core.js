804595a332950ba1dac6b878efaad2e1cb3c73cd
kangseok0427 <  >
2026-06-30 04:11:12 +0900

    gaon: 게임 실행 시 아무것도 안그려지는 버그 있음 core.js 읽고 수정

--- src/core.js
+++ src/core.js
@@ -1,27 +1,35 @@
-import Bullet from './obj/Bullet.js';
-import Star from './obj/Star.js';
 import GameManager from './GameManager.js';
 
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+let gameManager;
 
-const gameManager = new GameManager(canvas.width, canvas.height);
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    if (gameManager && typeof gameManager.resize === 'function') {
+        gameManager.resize(canvas.width, canvas.height);
+    }
+    if (gameManager && typeof gameManager.updateBounds === 'function') {
+        gameManager.updateBounds(canvas.width, canvas.height);
+    }
+}
 
 function init() {
+    resize();
+    gameManager = new GameManager(canvas.width, canvas.height);
     gameManager.initObjs();
 }
 
 function update() {
-    gameManager.update();
+    if (gameManager) gameManager.update();
 }
 
 function draw() {
+    if (!gameManager) return;
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     gameManager.draw(ctx);
 }
 
@@ -31,5 +39,6 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('resize', resize);
 init();
 loop();
