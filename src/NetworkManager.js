14b6bd617da4f93e87bfea8e34b0558ddb47de8a
kangseok0427 <  >
2026-07-01 21:26:24 +0900

    v2.0.1: websocket 관련 코드 전부 지우고 html이랑 css 클래스 이름

--- src/NetworkManager.js
+++ src/NetworkManager.js
@@ -2,26 +2,7 @@ import GameManager from './GameManager.js';
 
 export default class NetworkManager {
     constructor() {
-        this.socket = null;
-        this.initSocket();
-    }
-
-    initSocket() {
-        this.socket = io('http://localhost:3000'); // Replace with your server URL
-
-        this.socket.on('connect', () => {
-            console.log('Connected to the server');
-        });
-
-        this.socket.on('updateGameState', (data) => {
-            GameManager.updateState(data);
-        });
-    }
-
-    sendPlayerAction(action) {
-        if (this.socket.connected) {
-            this.socket.emit('playerAction', action);
-        }
+        // Initialize any non-websocket related properties here if needed
     }
 
     static updateState(data) {
